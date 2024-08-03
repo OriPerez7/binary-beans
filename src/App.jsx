0a88b7ff703d4010b6c700e330dbce2cdcf87f08
Orian Perez <dylanperezart@gmail.com>
2024-08-03 15:57:55 +0200

    Complete order form section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
+import Order from "./components/Order/Order";
 
 const App = () => {
   return <div className="overflow-x-hidden">
     <Hero />
+    <Products />
+    <Order />
   </div>;
 };
 
