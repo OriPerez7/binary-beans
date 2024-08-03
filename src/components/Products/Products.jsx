43f2070d0492aef61637aaa4f0842fbee131955f
Orian Perez <dylanperezart@gmail.com>
2024-08-03 07:37:53 +0200

    Complete products section

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { easeInOut, motion } from "framer-motion";
+import productsData from "./productsData";
+
+const cardVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 150,
+      damping: 10,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const containerVariants = {
+  hidden: { opacity: 1 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.6,
+      staggerChildren: 0.4,
+    },
+  },
+};
+
+const Products = () => {
+  return (
+    <div className="container my-16 space-y-4">
+      {/** Header */}
+      <div className="text-center max-w-lg mx-auto space-y-2">
+        <motion.h2
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            type: "spring",
+            stiffness: 150,
+            damping: 10,
+            delay: 0.2,
+          }}
+          className="text-3xl font-bold text-lightGray"
+        >
+          Check our <span className="text-primary">Coffee Delights</span>
+        </motion.h2>
+        <motion.p
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 150,
+            damping: 10,
+            delay: 0.6,
+          }}
+          className="text-sm opacity-50"
+        >
+          Energize your day with our expertly crafted brews, specially designed
+          to fuel your coding sessions and spark creativity. Enjoy the perfect
+          flavor.
+        </motion.p>
+      </div>
+
+      {/** Cards */}
+      <motion.div
+        variants={containerVariants}
+        initial="hidden"
+        whileInView={"visible"}
+        viewport={{ amount: 0.8 }}
+        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8"
+      >
+        {productsData.map((product) => (
+          <motion.div
+            variants={cardVariants}
+            className="text-center p-4 space-y-6"
+          >
+            <img
+              src={product.image}
+              alt={product.title}
+              className="img-shadow-two max-w-[200px] mx-auto hover:scale-110 duration-300 cursor-pointer"
+            />
+            <div className="space-y-2">
+              <h3 className="text-2xl font-bold text-primary">
+                {product.title}
+              </h3>
+              <p className="text-darkGray">{product.subtitle}</p>
+            </div>
+          </motion.div>
+        ))}
+      </motion.div>
+    </div>
+  );
+};
+
+export default Products;
---
+++ src/components/Products/productsData.js
@@ -0,0 +1,26 @@
+import coffee1 from "../../assets/coffee1.png";
+import coffee2 from "../../assets/coffee2.png";
+import coffee3 from "../../assets/coffee3.png";
+
+const productsData = [
+    {
+        id: 1,
+        image: coffee1,
+        title: "Americano",
+        subtitle: "A bold and smooth classic to kickstart your coding journey."
+    },
+    {
+        id: 2,
+        image: coffee2,
+        title: "Iced Cappuccino",
+        subtitle: "A refreshing twist on a creamy favorite, perfect for a cool break."
+    },
+    {
+        id: 3,
+        image: coffee3,
+        title: "Caramel Macchiato",
+        subtitle: "A sweet, indulgent treat to keep you inspired and energized."
+    },
+]
+
+export default productsData;
